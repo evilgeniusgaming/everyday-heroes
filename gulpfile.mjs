27ea9c2927868b8e35f7738535fdd4de32d3601b
Jeff Hitchcock <mail@command-center.org>
2023-06-17 11:25:53 -0700

    [#115] Update to new version of CLI, switch to CLI for build process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,25 +1,6 @@
 import gulp from "gulp";
 
-// import * as css from "./utils/css.mjs";
 import * as javascript from "./utils/javascript.mjs";
-import * as packs from "./utils/packs.mjs";
-
-
-// Style sheets compiling
-// export const buildCSS = gulp.series(css.compile);
 
 // Javascript compiling & linting
-export const buildJS = gulp.series(javascript.compile);
 export const lint = gulp.series(javascript.lint);
-
-// Compendium pack management
-export const cleanPacks = gulp.series(packs.clean);
-export const compilePacks = gulp.series(packs.compile);
-export const extractPacks = gulp.series(packs.extract);
-
-// Build all artifacts
-export const buildAll = gulp.parallel(
-	// css.compile,
-	javascript.compile,
-	packs.compile
-);
--- utils/javascript.mjs
+++ utils/javascript.mjs
@@ -2,8 +2,6 @@ import eslint from "gulp-eslint7";
 import gulp from "gulp";
 import gulpIf from "gulp-if";
 import mergeStream from "merge-stream";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import { rollup } from "rollup";
 import yargs from "yargs";
 
 
@@ -18,27 +16,6 @@ const parsedArgs = yargs(process.argv).argv;
  * @type {string[]}
  */
 const LINTING_PATHS = ["./everyday-heroes.mjs", "./code/"];
-
-
-/**
- * Compile javascript source files into a single output file.
- * @returns {*}
- *
- * - `gulp buildJS` - Compile all javascript files into into single file & build source maps.
- */
-async function compileJavascript() {
-	const bundle = await rollup({
-		input: "./everyday-heroes.mjs",
-		plugins: [nodeResolve()]
-	});
-	await bundle.write({
-		file: "./everyday-heroes-compiled.mjs",
-		format: "es",
-		sourcemap: true,
-		sourcemapFile: "everyday-heroes.mjs"
-	});
-}
-export const compile = compileJavascript;
 
 
 /**
